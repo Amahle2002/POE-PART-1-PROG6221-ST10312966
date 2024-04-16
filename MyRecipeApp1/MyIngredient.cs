c0891472d8ef24f95cb74f4ebe607c9b3e118c6c
Amahle2002 <128729889+Amahle2002@users.noreply.github.com>
2024-04-16 22:48:25 +0200

    CREATED THE MyIngredient CLASS

---
+++ MyRecipeApp1/MyIngredient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeApp1
+{
+    internal class MyIngredient
+    {
+    }
+}
